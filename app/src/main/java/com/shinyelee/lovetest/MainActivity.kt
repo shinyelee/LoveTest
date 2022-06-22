41f63aaf3b82081d942d7994b292c56f1d9eddce
shinyelee <shinye0213@naver.com>
2022-06-22 16:09:50 +0900

    noActionBar

--- app/src/main/java/com/shinyelee/lovetest/MainActivity.kt
+++ app/src/main/java/com/shinyelee/lovetest/MainActivity.kt
@@ -10,13 +10,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        navController = nav_host_fragment.findNavController()
-
         var navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        var navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
     }
 }
