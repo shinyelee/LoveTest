0e15a11c7e439a844a64906fcd8c9010b910bdf8
shinyelee <shinye0213@naver.com>
2022-06-22 16:04:55 +0900

    nav_graph.xml navController

--- app/src/main/java/com/shinyelee/lovetest/MainActivity.kt
+++ app/src/main/java/com/shinyelee/lovetest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.shinyelee.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        navController = nav_host_fragment.findNavController()
+
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        var navController = navHostFragment.navController
+
     }
 }
